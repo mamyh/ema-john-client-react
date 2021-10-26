668e9993a5c0cdfe212f452b08e4124794f535dc
mamyh <likhmaon@gmail.com>
2021-10-26 23:14:31 +0600

    from home

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # `this  is client side of ema john `
 --------------------------------------------
 ## `server side is blow in here`
+[server-side]https://github.com/mamyh/ema-john-server-node
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,27 +1,34 @@
 import { useState, useEffect } from 'react';
 import { getStoredCart } from '../utilities/fakedb';
 
-const useCart = products => {
+const useCart = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
+        const savedCart = getStoredCart();
+        fetch(`http://localhost:9000/products/byKeys`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(savedCart),
+        }).then(res => res.json()).then(data =>
+            console.log(data));
+        // if (products.length) {
+        //     const storedCart = [];
+        //     for (const key in savedCart) {
+        //         const addedProduct = products.find(product => product.key === key);
+        //         if (addedProduct) {
+        //             // set quantity
+        //             const quantity = savedCart[key];
+        //             addedProduct.quantity = quantity;
+        //             storedCart.push(addedProduct);
+        //         }
+        //     }
+        //     setCart(storedCart);
+        // }
 
-        if (products.length) {
-            const savedCart = getStoredCart();
-            const storedCart = [];
-            for (const key in savedCart) {
-                const addedProduct = products.find(product => product.key === key);
-                if (addedProduct) {
-                    // set quantity
-                    const quantity = savedCart[key];
-                    addedProduct.quantity = quantity;
-                    storedCart.push(addedProduct);
-                }
-            }
-            setCart(storedCart);
-        }
-
-    }, [products]);
+    }, []);
 
     return [cart, setCart];
 }
