f141edae039cf1e7ad0edc6d14d857cfd5b7e3e9
mamyh <likhmaon@gmail.com>
2021-10-27 14:17:20 +0600

    before lunch

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -4,29 +4,33 @@ import { getStoredCart } from '../utilities/fakedb';
 const useCart = () => {
     const [cart, setCart] = useState([]);
 
+    const savedCart = getStoredCart();
+
+
     useEffect(() => {
-        const savedCart = getStoredCart();
         fetch(`http://localhost:9000/products/byKeys`, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(savedCart),
-        }).then(res => res.json()).then(data =>
-            console.log(data));
-        // if (products.length) {
-        //     const storedCart = [];
-        //     for (const key in savedCart) {
-        //         const addedProduct = products.find(product => product.key === key);
-        //         if (addedProduct) {
-        //             // set quantity
-        //             const quantity = savedCart[key];
-        //             addedProduct.quantity = quantity;
-        //             storedCart.push(addedProduct);
-        //         }
-        //     }
-        //     setCart(storedCart);
-        // }
+            body: JSON.stringify(savedCart)
+        }).then(res => res.json()).then(data => {
+            let products = data;
+            if (products.length) {
+                const storedCart = [];
+                for (const key in savedCart) {
+                    const addedProduct = products.find(product => product.key === key);
+                    if (addedProduct) {
+                        // set quantity
+                        const quantity = savedCart[key];
+                        addedProduct.quantity = quantity;
+                        storedCart.push(addedProduct);
+                    }
+                }
+                setCart(storedCart);
+            }
+        });
+
 
     }, []);
 
